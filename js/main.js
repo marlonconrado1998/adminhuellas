4ca2f0b75b62de2ac611dbb99b76b5fede041977
NGM-PC\Neider Galofre <neider.galofre@zabud.co>
2018-03-16 13:44:48 -0500

    inicio retorna a inicio si no se encuentra una pagina

--- js/main.js
+++ js/main.js
@@ -2,14 +2,23 @@ var app = angular.module('app', [
     'ui.router'
 ]);
 
-app.config(function ($stateProvider) {
-    $stateProvider.state({
-    	name: 'form',
-        url: '/form',
-        templateUrl: 'pages/forms/basic_elements.html'
-    }).state({
-    	name: 'chart',
-        url: '/chart',
-        templateUrl: 'pages/charts/chartjs.html'
-    });    	
-});
+app.config(["$stateProvider", "$urlRouterProvider",
+    function ($stateProvider, $urlRouterProvider) {
+        
+        $urlRouterProvider.otherwise("/Inicio");
+
+        $stateProvider.state({
+            name: 'form',
+            url: '/form',
+            templateUrl: 'pages/forms/basic_elements.html'
+        }).state({
+            name: 'chart',
+            url: '/chart',
+            templateUrl: 'pages/charts/chartjs.html'
+        }).state({
+            name: 'Inicio',
+            url: '/Inicio',
+            templateUrl: 'pages/inicio.html'
+        });
+    }
+]);
