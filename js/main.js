e1f27fd5f03e083852fed7f4f60196393adeeb5b
Marlon Conrado <marlon.conrado@zabud.co>
2018-03-16 10:28:12 -0500

    no message

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var app = angular.module('app', [
+    'ui.router'
+]);
+
+app.config(function ($stateProvider) {
+    $stateProvider.state({
+    	name: 'form',
+        url: '/form',
+        templateUrl: 'pages/forms/basic_elements.html'
+    }).state({
+    	name: 'chart',
+        url: '/chart',
+        templateUrl: 'pages/charts/chartjs.html'
+    });    	
+});
