9b85f4f5a667cc5476e4958f3be1dc7c184c071b
NGM-PC\Neider Galofre <neider.galofre@zabud.co>
2018-03-20 17:20:19 -0500

    NO BAJAR

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ var app = angular.module('app', [
     'ui.router'
 ]);
 
-<<<<<<< HEAD
 app.config(["$stateProvider", "$urlRouterProvider",
     function ($stateProvider, $urlRouterProvider) {
         
