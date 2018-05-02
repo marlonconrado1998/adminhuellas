54e8f64cbc63f8cc32bb720d4c67626579ee22dc
NGM-PC\Neider Galofre <neider.galofre@zabud.co>
2018-05-02 17:49:35 -0500

    no message

---
+++ js/main.js
@@ -0,0 +1,38 @@
+var app = angular.module('app', [
+    'ui.router',
+    'ui.bootstrap',
+    'ngWizard'
+]);
+
+app.config(["$stateProvider", "$urlRouterProvider",
+    function ($stateProvider, $urlRouterProvider) {
+        
+        $urlRouterProvider.otherwise("/Inicio");
+
+        $stateProvider.state({
+            name: 'chart',
+            url: '/chart',
+            templateUrl: 'pages/charts/chartjs.html'
+        }).state({
+            name: 'Inicio',
+            url: '/Inicio',
+            templateUrl: 'pages/inicio.html'
+        }).state({
+            name: 'animales',
+            url: '/animales',
+            templateUrl: 'pages/animales.html'
+        }).state({
+            name: 'adopcion',
+            url: '/adopcion',
+            templateUrl: 'pages/adopcion.html'
+        }).state({
+            name: 'historias',
+            url: '/historias',
+            templateUrl: 'pages/historias.html'
+        }).state({
+            name: 'login',
+            url: '/login',
+            templateUrl: 'pages/login.html'
+        });  
+    }
+]);
