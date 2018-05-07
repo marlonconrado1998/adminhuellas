a5458c862a917bea313885e1d4525b67c1d541c4
Marlon Conrado <marlon.conrado@zabud.co>
2018-05-07 18:32:49 -0500

    not intregar

---
+++ js/controller/historialAnimal.controller.js
@@ -0,0 +1,11 @@
+(function() {
+    'use strict';
+
+    app.controller('HistorialCtrl', HistorialAnimal);
+
+    // HistorialAnimal.$inject = ['dependencies'];
+
+    function HistorialAnimal() {
+        
+    }
+})();
