89b91f5be6903ce26401919248fe50a5d5722f3f
NGM-PC\Neider Galofre <neider.galofre@zabud.co>
2018-05-17 11:44:04 -0500

    no message

--- js/config/constant.js
+++ js/config/constant.js
@@ -14,3 +14,16 @@ app.constant("FORMULARIO", {
 });
 
 app.constant("GeneralURL", "http://localhost/startadmin_back-end/webapis/api/");
+
+app.constant("printFile", {
+    print: function (elemento) {
+        var contenido = document.getElementById(elemento).innerHTML;
+        var contenidoOriginal = document.body.innerHTML;
+
+        document.body.innerHTML = contenido;
+
+        window.print();
+
+        document.body.innerHTML = contenidoOriginal;
+    }
+});
